cb1f5c781ac167012c3cbd8e43930142c78615ab
Jeevan Oraganti <jeevanoraganti75@gmail.com>
2024-10-22 16:51:58 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,73 @@
+
+<?php
+/*
+$books = [
+    [
+        'name' => 'Do Androids Dream of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'purchaseUrl' => 'http://example.com',
+        'releaseYear' => 1968
+    ],
+    [
+        'name' => 'The Shining',
+        'author' => 'Stephen King',
+        'purchaseUrl' => 'http://example.com',
+        'releaseYear' => 1977
+    ],
+    [
+        'name' => '1984',
+        'author' => 'George Orwell',
+        'purchaseUrl' => 'http://example.com',
+        'releaseYear' => 1949
+    ],
+    [
+        'name' => 'Brave New World',
+        'author' => 'Aldous Huxley',
+        'purchaseUrl' => 'http://example.com',
+        'releaseYear' => 1932
+    ],
+    [
+        'name' => 'Fahrenheit 451',
+        'author' => 'Ray Bradbury',
+        'purchaseUrl' => 'http://example.com',
+        'releaseYear' => 1953
+    ],
+    [
+        'name' => 'Neuromancer',
+        'author' => 'William Gibson',
+        'purchaseUrl' => 'http://example.com',
+        'releaseYear' => 1984
+    ],
+    [
+        'name' => 'Neuromancer',
+        'author' => 'William Gibson',
+        'purchaseUrl' => 'http://example.com',
+        'releaseYear' => 1984
+    ]
+];
+
+
+$filteredBooks = array_filter($books, function($book){
+    return $book['author'] === 'William Gibson';
+});
+
+require "index.view.php";
+*/
+
+
+
+
+
+
+$business = [
+    'name' => 'Laracasts',
+    'cost' => 15,
+    'categories' => ["Testing", "PHP", "JavaScript"]
+];
+
+
+function register($user) {
+
+}
+
+require "index.view.php";
